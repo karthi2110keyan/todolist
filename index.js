c1230cf5aa22ecb58d95ad437b370b583a41da14
Karthi <155168516+karthi2110keyan@users.noreply.github.com>
2024-04-11 08:22:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var divBox = document.querySelector(".list-container")
 const listItems=[]
 
 
-
-
+if(listItems===null){
+  console.log("hh")
+}
 
 
 button.addEventListener("click",function(){
@@ -16,17 +17,20 @@ button.addEventListener("click",function(){
 
  
  
- 
-
+ if(inputValue===""){
+  input.setAttribute("placeholder","Enter somethig on text feild")
+  input.style.border="1px solid red"
+ }else{
 
   if(inputValue.length<=30){
-    listItems.push(inputValue)
-
-    
+    listItems.push(inputValue) 
   }
+  input.removeAttribute("placeholder")
+  input.style.border="none"
+}
 
-if(listItems.length===0){
-  alert("delete the box")
+if(listItems.length===6){
+  alert("<h1>your list box has full</h1>")
 }else if(listItems.length===8){
   listItems.splice(0,1)
 }
@@ -44,6 +48,7 @@ input.value=""
 
 function render(){
 let todolist="";
+
  var i;
  for( i=0;i<listItems.length;i++){
 
@@ -56,11 +61,16 @@ const  todo= listItems[i]
     const html = `<div  class="parabox"><p class="para">`+todo+`</p><div class="button-box"><button class="delete" onclick="listItems.splice(${i},1);render() 
     ">x</button><button class="editbox"></button></div></div>`
 
+
+    if(html.length===191){
+      setTimeout(()=>{
+        html=""
+      },100)
+    }else{
     todolist += html
     
-
- 
     divBox.innerHTML= todolist
+    }
   
   var edit = document.querySelector(".editbox")
   var col =document.querySelector(".parabox")
@@ -79,4 +89,3 @@ edit.addEventListener("click",function(){
 
 
  }
-
